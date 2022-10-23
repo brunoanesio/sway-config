130c91098890b78eed437302a57fc326071a1b12
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-10-23 02:42:13 -0300

    Update waybar script

--- waybar.sh
+++ waybar.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 killall -q "waybar"
-killall -q "polkit-gnome-authentication-agent-1"
 
 while pgrep -x waybar >/dev/null; do sleep 1; done
 
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 waybar -c "$HOME"/.config/sway/waybar/config -s "$HOME"/.config/sway/waybar/style.css
